f8184c3fb1b0c46b6c6033b9cdb231faebf21c30
79255@DESKTOP-A17MI81 <Don20034562@bk.ru>
2023-02-24 21:53:28 +0300

    HW_1_2

---
+++ HW_1/1_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите 1 значение: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2 значение: ");
+int B = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 3 значение: ");
+int C = int.Parse(Console.ReadLine()!);
+ int Max = A;
+if (Max < B)
+{
+    Max = B;
+}
+if (Max < C)
+{
+    Max = C;
+}
+Console.Write("Максимальное чисто= ");
+Console.WriteLine(Max);
+

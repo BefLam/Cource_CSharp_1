aac3baeee9225c2ba8a3507b68f4f7298e0efcf7
79255@DESKTOP-A17MI81 <Don20034562@bk.ru>
2023-03-03 13:36:48 +0300

    1_1

---
+++ HW_2/1_1/Program.cs
@@ -0,0 +1,11 @@
+﻿int Num(int A)
+{
+    Console.WriteLine(A);
+    int num1 = A / 10 % 10;
+    int num2 = 2;
+    if (num1 > num2) return num1;
+    return num1;
+}
+
+int result = Num(new Random().Next(100, 1000));
+Console.WriteLine(result);

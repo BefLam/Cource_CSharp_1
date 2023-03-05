29360c6465a11cb245a4b51559808ed30329d94c
79255@DESKTOP-A17MI81 <Don20034562@bk.ru>
2023-03-05 20:20:46 +0300

    HW_3

---
+++ HW_3/3_1/Program.cs
@@ -0,0 +1,33 @@
+﻿int Num(int A)
+{
+    Console.WriteLine(A);
+    int num1 = A / 10000;
+    int num2 = A % 10 ;
+    int num3 = A / 1000 % 10;
+    int num4 = A / 10 % 10;
+    int True = 1;
+    int False = 2;
+    if (num1 == num2 && num3 == num4) return True;
+    return False;
+}
+
+int Res = Num(new Random().Next(10000, 100000));
+if (Res < 2)
+{
+    Console.WriteLine("Yes");
+}
+else Console.WriteLine("No");
+
+
+//Второй вариант решения данной задачи!
+
+//void Palindrom (string numbers)
+//{
+   
+//    if (numbers[0] == numbers[4] && numbers[1] == numbers[3])
+//    {
+//        Console.WriteLine("Да");
+//    }
+//    else Console.WriteLine("Нет");
+//}
+//Palindrom(Console.ReadLine());

8fe5ce29d80f126a6dfe18ffb39877bac8a74a84
79255@DESKTOP-A17MI81 <Don20034562@bk.ru>
2023-03-06 11:12:09 +0300

    4_4

---
+++ Lesson_4/4_4/Program.cs
@@ -0,0 +1,11 @@
+﻿void massive(int mas)
+{
+    int[] numbers = new int[mas];
+    for (int i = 0; i < mas; i++)
+    {
+        numbers[i] = new Random().Next(2);
+        Console.Write(numbers[i]);
+    }
+
+}
+massive(8);

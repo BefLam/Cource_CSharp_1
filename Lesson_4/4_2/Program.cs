047d952e4261baa029719f3a7789e9b579677363
79255@DESKTOP-A17MI81 <Don20034562@bk.ru>
2023-03-06 10:49:39 +0300

    4_2

---
+++ Lesson_4/4_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// 1. Напишите программу, которая принимает
+//    на вход число и выдаёт количество цифр в числе.
+int lengthNum(int num)
+{
+    int a = 0;
+    for (int i = 1; num > 0; i++)
+       {
+        num = num / 10;
+        a++;
+       }
+    return a;   
+}
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int result = lengthNum(num);
+Console.WriteLine(result);

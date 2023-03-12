20f1f7a4b55141b3c52dab68b09431b16c5174a7
79255@DESKTOP-A17MI81 <Don20034562@bk.ru>
2023-03-12 06:34:55 +0300

    5_2

---
+++ HW_5/5_2/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+
+void Posit(int[] arr)
+{
+    int Col = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0)
+            Col += arr[i];
+    }
+    Console.WriteLine($"сумма нечётных чисел в массиве: {Col}");
+}
+
+
+Console.WriteLine("Введите длину массива: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите стартовое значение: ");
+int start = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите конечное значение: ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+Posit(mass);

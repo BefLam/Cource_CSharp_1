01b5a00bff9c79bb379b1df57e753f9819f3343b
79255@DESKTOP-A17MI81 <Don20034562@bk.ru>
2023-03-12 06:18:38 +0300

    5_1

---
+++ HW_5/5_1/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(1000, 10000);
+    return arr;
+}
+
+void Posit(int[] arr)
+{
+    int Col = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            Col += 1;
+    }
+    Console.WriteLine($"количество чётных чисел в массиве: {Col}");
+}
+
+
+Console.WriteLine("Введите длину массива: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num);
+Print(mass);
+Posit(mass);
+

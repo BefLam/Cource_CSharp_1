d497b8bb88a30c6117873994a02200397a873a86
79255@DESKTOP-A17MI81 <Don20034562@bk.ru>
2023-03-13 11:04:23 +0300

    6_3

---
+++ Lesson_6/6_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// 2. Напишите программу, которая будет
+//    преобразовывать десятичное число в двоичное.
+
+string ConvertBin(int num)
+{
+    string result = "";
+    while (num < 0)
+    {
+        result = num % 2 + result;
+        num /= 2;
+    }
+    
+    return result;
+}
+int A = int.Parse(Console.ReadLine()!);
+string res2 = ConvertBin(A);
+Console.WriteLine(res2);

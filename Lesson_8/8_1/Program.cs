4eb52c6a77327d8a72a98f2f9ba686d28cc92e50
79255@DESKTOP-A17MI81 <Don20034562@bk.ru>
2023-03-20 10:48:28 +0300

    8_1

---
+++ Lesson_8/8_1/Program.cs
@@ -0,0 +1,10 @@
+﻿void ReplaceIndex(int[,] array)
+{
+    int ArrayRow = array.GetLength(0);
+    int ArrayColumn = array.GetLength(1);
+    
+    for (int j = 0; j < ArrayColumn; j++)
+    {
+        (array[0,j] , array[ArrayRow - 1 ,j]) = (array[ArrayRow - 1, j], array[0,j]);
+    }
+}

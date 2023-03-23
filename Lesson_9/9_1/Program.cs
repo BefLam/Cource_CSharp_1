8476c82d9facffea9850af1fe07929a4d77957f5
79255@DESKTOP-A17MI81 <Don20034562@bk.ru>
2023-03-23 11:15:25 +0300

    9_1

---
+++ Lesson_9/9_1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задайте занчение N. Напишите программу, которая
+// выведет все натуральные числав промежутке
+// от 1 до N при помощи рекурсии.
+
+void SeriesNums(int N)
+{
+
+    if (N == 0) return;
+    SeriesNums(N - 1);
+    Console.Write($"{N}, ");
+
+}
+
+SeriesNums(int.Parse(Console.ReadLine()!));

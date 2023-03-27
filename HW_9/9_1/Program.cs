532b6fe4e8aee51f8fe0ee26e7daf2266d0a5617
79255@DESKTOP-A17MI81 <Don20034562@bk.ru>
2023-03-27 13:03:37 +0300

    9_1

---
+++ HW_9/9_1/Program.cs
@@ -0,0 +1,19 @@
+﻿void Rec( int M, int N)
+{
+    if (M > N) return;
+    else if (N % 2 == 0)
+    {
+        Rec(M, N - 2);
+        Console.Write($"{N}, ");
+    }
+    else if (N % 2 != 0)
+    {
+        N -= 1;
+        Rec(M, N - 2);
+        Console.Write($"{N}, ");
+    }
+    
+}
+int M = int.Parse(Console.ReadLine()!);
+int N = int.Parse(Console.ReadLine()!);
+Rec(M, N);
